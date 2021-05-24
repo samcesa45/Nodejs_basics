ca6a3ed1b7039841ab2b57b37560acfcd84ba34f
samcesa45 <samcesa45@gmail.com>
2021-05-24 07:16:41 -0700

    added build directory

---
+++ build/precache-manifest.4d54c8151d438a9bff8a1d313cfd0490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da03cc6fcaa5ecb262d731dea5f1cd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab4a108e139999c1a607",
+    "url": "/static/css/main.99a5a97d.chunk.css"
+  },
+  {
+    "revision": "fa67e3a3f0994c839536",
+    "url": "/static/js/2.41339d4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41339d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4a108e139999c1a607",
+    "url": "/static/js/main.56d3f893.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  }
+]);
